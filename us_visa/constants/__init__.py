bcf6ac8159e25b02ed20ec2c03bf75007098aab8
Nihal7778 <nihal77778@gmail.com>
2025-02-07 20:49:19 -0500

    data validation added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -14,6 +14,9 @@ MODEL_FILE_NAME = "model.pkl"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -24,4 +27,11 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 #using 80% for training and 20% for testing
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
